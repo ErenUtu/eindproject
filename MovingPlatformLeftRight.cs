4c0a8b48271a645b72945e248b6e5464d439cc37
ErenUtu <112859357+ErenUtu@users.noreply.github.com>
2025-01-31 14:07:52 +0100

    Add files via upload

--- MovingPlatformLeftRight.cs
+++ MovingPlatformLeftRight.cs
@@ -2,23 +2,23 @@ using UnityEngine;
 
 public class MovingPlatformLeftRight : MonoBehaviour
 {
-    public float moveSpeed = 2f; // Speed of the platform
-    public float moveRange = 3f; // How far it moves left and right
-    private Vector3 startPos;
+    public float moveSpeed = 2f; // Snelheid van het platform
+    public float moveRange = 3f; // Hoe ver het platform naar links en rechts beweegt
+    private Vector3 startPos; // De beginpositie van het platform
 
     void Start()
     {
-        startPos = transform.position; // Store the initial position
+        startPos = transform.position; // Sla de beginpositie op
     }
 
     void Update()
     {
-        MovePlatform();
+        MovePlatform(); // Beweeg het platform elke frame
     }
 
     void MovePlatform()
     {
-        // Moves the platform left and right using Mathf.PingPong
+        // Beweeg het platform naar links en rechts met behulp van Mathf.PingPong
         transform.position = new Vector3(startPos.x + Mathf.PingPong(Time.time * moveSpeed, moveRange), transform.position.y, transform.position.z);
     }
 }
